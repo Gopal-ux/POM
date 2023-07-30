14dc22137f6f057bcd35997be6c35109b7b91ac3
Gopal-ux <gkulkarni0509@gmail.com>
2023-07-30 12:02:25 +0530

    Parallel execution and Reports

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    reportFilename: '[name].html',
+    overwrite: true,
+    html: true,
+    json: false,
+  },
   e2e: {
 
      "baseUrl" : "https://way2automation.com/angularjs-protractor/banking/#/login",  
@@ -25,3 +33,11 @@ module.exports = defineConfig({
 
 
 });
+
+//How Parallel works
+// How it works
+// 🔍 - Search for existing Cypress tests
+// 📄 - Read (if exists) a weight file
+// ⚖️ - Split spec files into different threads
+// ⚙️ - For each thread it runs the Cypress command you've passed as argument
+// 📈 - Wait for all threads to finish and collects the result in a single report
